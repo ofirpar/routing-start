c7b3010e62dc273e40f653b242dc26017fe717ed
Ofir <tabu90@gmail.com>
2017-06-12 15:28:55 +0300

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+
+import {NgModule} from "@angular/core";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {EditServerComponent} from "./servers/edit-server/edit-server.component";
+import {HomeComponent} from "./home/home.component";
+import {UsersComponent} from "./users/users.component";
+import {ServersComponent} from "./servers/servers.component";
+import {ServerComponent} from "./servers/server/server.component";
+import {UserComponent} from "./users/user/user.component";
+import {Routes, RouterModule} from "@angular/router";
+import {AuthGuard} from "./auth-guard.service";
+import {CanDeactivateGuard} from './servers/edit-server/can-deactivate-guard.service';
+import {ErrorPageComponent} from "./error-page/error-page.component";
+import {ServerReslover} from "./servers/server/server-resolver.service";
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Home', component: HomeComponent },
+  { path: 'Users', component: UsersComponent, children: [
+    { path: ':id/:name', component: UserComponent },
+  ] },
+  { path: 'Servers',
+    //canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children: [
+    { path: ':id', component: ServerComponent, resolve: {server: ServerReslover}},
+    { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]},
+  ] },
+  // { path: 'not-found', component: PageNotFoundComponent},
+  { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'}},
+  { path: '**', redirectTo: '/not-found'},
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes, {useHash: true})
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
